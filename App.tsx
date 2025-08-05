64a986a5e45b3c89aae170ed358a98c15e070ec6
wochoa94 <ochoa.werner94@gmail.com>
2025-08-04 22:43:56 -0600

    Implement Login Conversion Requirements

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import { View, Text } from 'react-native';
 
 // Import your screens
 import LoginScreen from './src/screens/LoginScreen';
